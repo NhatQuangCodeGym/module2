17e8cf3867e6c503088168da220b6b47f7b4c422
NhatQuangCodeGym <lnquang0109@gmail.com>
2021-11-10 23:31:47 +0700

    done

---
+++ Stop-Watch/src/StopWatch.java
@@ -0,0 +1,31 @@
+import java.time.LocalTime
+public class StopWatch {
+    private LocalTime startTime, endTime;
+    public StopWatch(){
+        startTime=LocalTime.now();
+    }
+
+    public StopWatch(LocalTime startTime,LocalTime endTime){
+        this.startTime=startTime;
+        this.endTime=endTime;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public start(){
+         startTime=LocalTime.now();
+    }
+    public stop(){
+        endTime=LocalTime.now();
+    }
+    public int getElapsedTime(){
+        int miliSecond = ((endTime.getHour()-startTime.getHour())*3600 + (endTime.getMinute()-startTime.getMinute())*60 + (endTime.getSecond()-startTime.getSecond())*1000);
+        return miliSecond;
+    }
+}
