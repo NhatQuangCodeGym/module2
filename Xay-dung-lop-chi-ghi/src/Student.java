3d2ccc2473d91282260f6d8bf3b75ba2261e72b0
NhatQuangCodeGym <lnquang0109@gmail.com>
2021-11-11 00:04:10 +0700

    done

---
+++ Xay-dung-lop-chi-ghi/src/Student.java
@@ -0,0 +1,26 @@
+public class Student {
+    private String name = "John";
+    private String lop = "CO2";
+    public Student(){
+        name = "John";
+        lop = "C02";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLop() {
+        return lop;
+    }
+
+    public void setLop(String lop) {
+        this.lop = lop;
+    }
+}
+
+
---
+++ Xay-dung-lop-chi-ghi/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        st1.setName("Nghia");
+        st1.setLop("C03");
+        System.out.println(st1.getName());
+        System.out.println(st1.getLop());
+    }
+}
