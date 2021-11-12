99509728db6cfc8197d09dfa7edcc8a71083384d
NhatQuangCodeGym <lnquang0109@gmail.com>
2021-11-12 14:33:14 +0700

    done

---
+++ 2D-3Dpoint/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D first2D = new Point2D(9.0f,7.0f);
+        System.out.println(first2D);
+        Point3D first3D = new Point3D(4.0f,3.0f,3.0f);
+        System.out.println(first3D);
+    }
+}
