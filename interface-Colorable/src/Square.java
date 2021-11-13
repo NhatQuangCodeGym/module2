a8f16b57465d406ec66844c864329ff2cebad1a3
NhatQuangCodeGym <lnquang0109@gmail.com>
2021-11-13 23:08:13 +0700

    done

---
+++ interface-Colorable/src/Square.java
@@ -0,0 +1,37 @@
+public class Square extends Shape implements Colorable {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public Square(String color, boolean filled, double side) {
+        super(color, filled);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+    public double getArea(){
+        return side * side;
+    }
+    public double getPerimeter(){
+        return 4*side;
+    }
+    @Override
+    public String toString() {
+        return "A Square with side ="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+    @Override
+    public void howToColor(){
+        System.out.println("Color all four sides");
+    }
+}
